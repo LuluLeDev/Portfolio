0b55d622d6d67681b4c1c27c9be384f1136b22a4
LuluLeDev <lucas20037@icloud.com>
2023-03-12 18:58:15 +0100

    avancement page d'acceuil + JS

---
+++ script/meDisplay.js
@@ -0,0 +1,9 @@
+function showDiv(){
+    var div = document.getElementsByClassName("MeDisplay");
+    div.style.display = "none";
+    // if(getComputedStyle(div).display == "none"){
+    //   div.style.display = "flex";
+    // } else {
+    //   div.style.display = "none";
+    // }
+}
