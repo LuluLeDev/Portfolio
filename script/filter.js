2e967f451153e86ecee8ec05fc9c5866b79be596
LuluLeDev <lucas20037@icloud.com>
2023-07-27 04:08:18 +0200

    je sais plus ce que j'ai fait

---
+++ script/filter.js
@@ -0,0 +1,45 @@
+function filter(id){
+    var divCarousel = document.getElementById("carouselID");
+    var divWeb = document.getElementById("webDiv");
+    var divProg = document.getElementById("progDiv");
+    var divOther = document.getElementById("otherDiv");
+    var divFilter;
+    switch(id){
+        case "discoverFilter":
+            divFilter = getComputedStyle(divCarousel, null);
+            if(divFilter.getPropertyValue("display") == "none"){
+                divCarousel.style.display = "flex";
+                divWeb.style.display = "none";
+                divProg.style.display = "none";
+                divOther.style.display = "none";
+            }
+            break;
+        case "webFilter":
+            divFilter = getComputedStyle(divWeb, null);
+            if(divFilter.getPropertyValue("display") == "none"){
+                divCarousel.style.display = "none";
+                divWeb.style.display = "flex";
+                divProg.style.display = "none";
+                divOther.style.display = "none";
+            }
+            break;
+        case "progFilter":
+            divFilter = getComputedStyle(divProg, null);
+            if(divFilter.getPropertyValue("display") == "none"){
+                divCarousel.style.display = "none";
+                divWeb.style.display = "none";
+                divProg.style.display = "flex";
+                divOther.style.display = "none";
+            }
+            break;
+        case "otherFilter":
+            divFilter = getComputedStyle(divOther, null);
+            if(divFilter.getPropertyValue("display") == "none"){
+                divCarousel.style.display = "none";
+                divWeb.style.display = "none";
+                divProg.style.display = "none";
+                divOther.style.display = "flex";
+            }
+            break;
+    }
+}
