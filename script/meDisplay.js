48a8da22e4951dd00faeb258d19be7515c16590e
LuluLeDev <lucas20037@icloud.com>
2023-07-27 05:35:44 +0200

    modif script pour coordonnées les différents affichages

--- script/meDisplay.js
+++ script/meDisplay.js
@@ -1,12 +1,58 @@
 function showDiv(){
     var divMe = document.getElementById("MeDisplayID");
     var divCarousel = document.getElementsByClassName("Carousel");
-    var divProp = getComputedStyle(divMe, null);
-    if(divProp.getPropertyValue("display") == "none"){
-       divMe.style.display = "flex";
-       divCarousel[0].style.visibility = "hidden";
-    } else {
-      divMe.style.display = "none";
-      divCarousel[0].style.visibility = "visible";
+    var divWeb = document.getElementById("webDiv");
+    var divProg = document.getElementById("progDiv");
+    var divOther = document.getElementById("otherDiv");
+    var divShow = getComputedStyle(divMe, null);
+    var divToShow;
+    var tabDiv = [divWeb,divProg,divOther];
+
+    divToShow = getComputedStyle(divWeb, null);
+    if(divToShow.getPropertyValue("display") == "flex"){
+      if(divShow.getPropertyValue("display") == "none"){
+        divMe.style.display = "flex";
+        divWeb.style.visibility = "hidden";
+      } 
+      else {
+        divMe.style.display = "none";
+        divWeb.style.visibility = "visible";
+      }
     }
+    divToShow = getComputedStyle(divProg, null);
+    if(divToShow.getPropertyValue("display") == "flex"){
+      if(divShow.getPropertyValue("display") == "none"){
+        divMe.style.display = "flex";
+        divProg.style.visibility = "hidden";
+      } 
+      else {
+        divMe.style.display = "none";
+        divProg.style.visibility = "visible";
+      }
+    }
+    divToShow = getComputedStyle(divOther, null);
+    if(divToShow.getPropertyValue("display") == "flex"){
+      if(divShow.getPropertyValue("display") == "none"){
+        divMe.style.display = "flex";
+        divOther.style.visibility = "hidden";
+      } 
+      else {
+        divMe.style.display = "none";
+        divOther.style.visibility = "visible";
+      }
+    }
+    
+
+    // tabDiv.forEach(function(item){
+    //   console.log(item);
+    //   divShow = getComputedStyle(item, null);
+    //   if(divShow.getPropertyValue("display") == "flex"){
+    //     item.style.visibility = "hidden";
+    //     divMe.style.display = "flex";
+    //   }
+    //   else{
+    //     item.style.visibility = "flex";
+    //     divMe.style.visibility = "hidden";
+    //   }
+    // });
 }
