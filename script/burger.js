69c4700db0f6427977c662e07afb0fea0cf304db
LuluLeDev <lucas20037@icloud.com>
2023-08-02 16:40:26 +0200

    burger menu termine

--- script/burger.js
+++ script/burger.js
@@ -1,23 +1,37 @@
-$(document).ready(function () {
-  //Menu button on click event
-  $("#burgerButton").on("click", function () {
-    // Toggles a class on the menu button to transform the burger menu into a cross
-    $(".burgerLine:nth-of-type(1)").toggleClass(
-      "burgerLine--1"
-    );
-    $(".burgerLine:nth-of-type(2)").toggleClass(
-      "burgerLine--2"
-    );
-    $(".burgerLine:nth-of-type(3)").toggleClass(
-      "burgerLine--3"
-    );
-
-    // Toggles a class that slides the menu into view on the screen
-    $("#burgerMenu").toggleClass("burgerMenu--open");
-    return false;
-  });
-});
-
 function showBurgerMenu(){
-  
+  var burgerMenu = document.getElementById("burgerMenu");
+  var menuShow = getComputedStyle(burgerMenu, null);
+  var burgerLine1 = document.getElementById("line1");
+  var burgerLine2 = document.getElementById("line2");
+  var burgerLine3 = document.getElementById("line3");
+  var name = document.getElementById("nameTitle");
+
+  if(menuShow.getPropertyValue("display") == "none"){
+    burgerMenu.style.display = "flex";
+    name.style.display = "none";
+
+    burgerLine1.style.transform = "rotate(45deg)";
+    burgerLine1.style.top = "13px";
+    burgerLine1.style.position = "absolute";
+    
+    burgerLine2.style.display = "none";
+
+    burgerLine3.style.transform = "rotate(135deg)";
+    burgerLine3.style.top = "13px";
+    burgerLine3.style.position = "absolute";
+  }
+  else{
+    burgerMenu.style.display = "none";
+    name.style.display = "flex";
+
+    burgerLine1.style.transform = "rotate(180deg)";
+    burgerLine1.style.top = "13px";
+    burgerLine1.style.position = "static";
+
+    burgerLine2.style.display = "block";
+    
+    burgerLine3.style.transform = "rotate(180deg)";
+    burgerLine3.style.top = "13px";
+    burgerLine3.style.position = "static";
+  }
 }
--- script/filter.js
+++ script/filter.js
@@ -4,6 +4,14 @@ function filter(id){
     var divProg = document.getElementById("progDiv");
     var divOther = document.getElementById("otherDiv");
     var divMe = document.getElementById("MeDisplayID");
+
+    var burgerMenu = document.getElementById("burgerMenu");
+    var burgerMenuOn = getComputedStyle(burgerMenu, null);
+    var name = document.getElementById("nameTitle");
+    var burgerLine1 = document.getElementById("line1");
+    var burgerLine2 = document.getElementById("line2");
+    var burgerLine3 = document.getElementById("line3");
+
     var divMeShow = getComputedStyle(divMe, null);
     if(divMeShow.getPropertyValue("display") == "none"){
         var divFilter;
@@ -44,6 +52,20 @@ function filter(id){
                     divOther.style.display = "flex";
                 }
                 break;
+            }
+            if(burgerMenuOn.getPropertyValue("display") == "flex"){
+                burgerMenu.style.display = "none";
+                name.style.display = "flex";
+
+                burgerLine1.style.transform = "rotate(180deg)";
+                burgerLine1.style.top = "13px";
+                burgerLine1.style.position = "static";
+
+                burgerLine2.style.display = "block";
+                
+                burgerLine3.style.transform = "rotate(180deg)";
+                burgerLine3.style.top = "13px";
+                burgerLine3.style.position = "static";
             }
     }
 }
