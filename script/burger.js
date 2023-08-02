5e05a377336f92bd889d00c70f8bb78d73c71e96
LuluLeDev <lucas20037@icloud.com>
2023-08-02 02:36:54 +0200

    commencement burger menu

--- script/burger.js
+++ script/burger.js
@@ -1,8 +1,23 @@
-var burgerMenu = document.getElementById('burgerMenu');
+$(document).ready(function () {
+  //Menu button on click event
+  $("#burgerButton").on("click", function () {
+    // Toggles a class on the menu button to transform the burger menu into a cross
+    $(".burgerLine:nth-of-type(1)").toggleClass(
+      "burgerLine--1"
+    );
+    $(".burgerLine:nth-of-type(2)").toggleClass(
+      "burgerLine--2"
+    );
+    $(".burgerLine:nth-of-type(3)").toggleClass(
+      "burgerLine--3"
+    );
 
-var overlay = document.getElementById('menu');
-
-burgerMenu.addEventListener('click', function() {
-  this.classList.toggle("close");
-  overlay.classList.toggle("overlay");
+    // Toggles a class that slides the menu into view on the screen
+    $("#burgerMenu").toggleClass("burgerMenu--open");
+    return false;
+  });
 });
+
+function showBurgerMenu(){
+  
+}
